93683a0d3a39b8cf5353c05e1ac7bf2723dd1add
Juan Florez Burgos <juanflorezburgos@MacBook-Air-de-Juan.local>
2025-02-08 17:23:24 -0500

    second commit

--- machine.js
+++ machine.js
@@ -1,1 +1,13 @@
+const coins = document.querySelector('#coins').innerText = "0"
+const insertCoin = document.querySelector('#insertCoin')
 
+if (coins === "0") {
+    document.querySelector('#panel').innerHTML = "Por favor, inserta uma moneda para comprar."
+}
+
+function insertarMoneda() {
+    document.querySelector('#coins').innerText = "2000"
+    
+}
+
+insertCoin.addEventListener('click', insertarMoneda)
